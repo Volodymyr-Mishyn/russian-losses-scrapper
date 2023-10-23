0e7ba272f5d0ebf906b361d7c2a3fa073c9d78c5
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-10-23 13:42:16 +0300

    -oryx formatter first steps

--- src/formatters/oryx-formatter.ts
+++ src/formatters/oryx-formatter.ts
@@ -12,8 +12,39 @@ const categoryToActual: Array<[string, string]> = [
   ['damagedAndAbandoned', 'damaged and abandoned'],
 ];
 const categoryMap = new Map<string, string>(categoryToActual);
-
+//TODO: finish this class
 export class OryxFormatter extends Formatter<OryxScrapResult, OryxFormatResult> {
+  private _processTitleName(title: string): string {
+    const nameRegex = /(.+?)\s+-/;
+    const nameMatch = title.match(nameRegex);
+    if (!nameMatch || !nameMatch[1]) {
+      throw new Error('Name not found.');
+    }
+    return nameMatch[1].trim();
+  }
+
+  private _processTitleStat(title: string): OryxStat {
+    const statsRegex = /(\d+)/g;
+    const statsMatches = title.match(statsRegex);
+    if (!statsMatches || statsMatches.length < 5) {
+      throw new Error('Statistics not found');
+    }
+    return {
+      all: parseInt(statsMatches[0], 10),
+      destroyed: parseInt(statsMatches[1], 10),
+      damaged: parseInt(statsMatches[2], 10),
+      abandoned: parseInt(statsMatches[3], 10),
+      captured: parseInt(statsMatches[4], 10),
+    };
+  }
+
+  private _processTitle(title: string): OryxStatRoot {
+    return {
+      name: this._processTitleName(title),
+      statistics: this._processTitleStat(title),
+    };
+  }
+
   protected innerFormat(): Promise<OryxFormatResult> {
     throw new Error('Method not implemented.');
   }
